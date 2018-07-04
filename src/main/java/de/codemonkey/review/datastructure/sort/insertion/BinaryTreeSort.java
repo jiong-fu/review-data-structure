9d78ab9ea2b51d31ed393b8d3aa1fb4d734cddad
Jiong Fu <fujiong@outlook.com>
2018-07-04 23:20:16 +0200

    add binary tree sort

--- src/main/java/de/codemonkey/review/datastructure/sort/insertion/BinaryTreeSort.java
+++ src/main/java/de/codemonkey/review/datastructure/sort/insertion/BinaryTreeSort.java
@@ -2,12 +2,110 @@ package de.codemonkey.review.datastructure.sort.insertion;
 
 import de.codemonkey.review.datastructure.sort.Sort;
 
+import static de.codemonkey.review.datastructure.sort.ArithmeticUtils.isSmallerThan;
+
 public class BinaryTreeSort<N extends Number> implements Sort<N> {
 
     @Override
-    public N[] sort(N[] unordered) {
+    public N[] sort(N[] array) {
+        TreeNode root = generateBinaryTree(array);
+        return traverseBinaryTree(root);
+    }
 
-        return null;
+    /**
+     * Generate binary tree by iterating the array
+     *
+     * @param array the array to be iterated
+     * @return the resulting binary tree as root tree node
+     */
+    private TreeNode generateBinaryTree(N[] array) {
+        TreeNode root = null;
+        for (int i = 0; i < array.length; i++) {
+            root = insertIntoBinaryTree(array[i], root);
+        }
+        return root;
+    }
+
+    /**
+     * Insert element into the binary tree
+     *
+     * @param element the element to be inserted
+     * @param root    the current root of the binary tree
+     * @return the resulting binary tree as root tree node
+     */
+    private TreeNode insertIntoBinaryTree(N element, TreeNode root) {
+        if (root == null) {
+            return new TreeNode(element);
+        }
+
+        if (isSmallerThan(element, root.data)) {
+            if (root.leftChild == null) {
+                root.createLeftChild(element);
+            } else {
+                insertIntoBinaryTree(element, root.leftChild);
+            }
+        } else {
+            if (root.rightChild == null) {
+                root.createRightChild(element);
+            } else {
+                insertIntoBinaryTree(element, root.rightChild);
+            }
+        }
+
+        return root;
+    }
+
+    /**
+     * In-order traverse the binary tree
+     *
+     * @param root the current root of the binary tree
+     * @return the in-order traversed tree nodes as array
+     */
+    private N[] traverseBinaryTree(TreeNode root) {
+        N[] leftChildArray = null, rightChildArray = null;
+        int leftChildLength = 0, rightChildLength = 0;
+        if (root.leftChild != null) {
+            leftChildArray = traverseBinaryTree(root.leftChild);
+            leftChildLength = leftChildArray.length;
+        }
+        if (root.rightChild != null) {
+            rightChildArray = traverseBinaryTree(root.rightChild);
+            rightChildLength = rightChildArray.length;
+        }
+
+        Number[] result = new Number[leftChildLength + 1 + rightChildLength];
+        int currentIndex = 0;
+        for (int i = 0; i < leftChildLength; i++, currentIndex++) {
+            result[currentIndex] = leftChildArray[i];
+        }
+        result[currentIndex++] = root.data;
+        for (int i = 0; i < rightChildLength; i++, currentIndex++) {
+            result[currentIndex] = rightChildArray[i];
+        }
+
+        return (N[]) result;
+    }
+
+    /**
+     * Tree node of an unbalanced binary tree
+     */
+    private class TreeNode {
+
+        private TreeNode leftChild;
+        private N data;
+        private TreeNode rightChild;
+
+        TreeNode(N data) {
+            this.data = data;
+        }
+
+        void createLeftChild(N data) {
+            this.leftChild = new TreeNode(data);
+        }
+
+        void createRightChild(N data) {
+            this.rightChild = new TreeNode(data);
+        }
     }
 
 }
---
+++ src/test/java/de/codemonkey/review/datastructure/sort/insertion/BinaryTreeSortTest.java
@@ -0,0 +1,16 @@
+package de.codemonkey.review.datastructure.sort.insertion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class BinaryTreeSortTest {
+
+    @Test
+    public void testBinaryTreeSort() {
+        Number[] array = {6, 5, 3, 1, 8, 7, 2, 4};
+        Number[] expected = {1, 2, 3, 4, 5, 6, 7, 8};
+        assertArrayEquals(expected, new BinaryTreeSort().sort(array));
+    }
+
+}
