fc06e1c22c2abab77918dc41115dbd4212d0d661
Jiong Fu <fujiong@outlook.com>
2018-07-06 00:19:45 +0200

    add comb sort

--- src/main/java/de/codemonkey/review/datastructure/sort/exchange/CombSort.java
+++ src/main/java/de/codemonkey/review/datastructure/sort/exchange/CombSort.java
@@ -2,12 +2,27 @@ package de.codemonkey.review.datastructure.sort.exchange;
 
 import de.codemonkey.review.datastructure.sort.Sort;
 
+import static de.codemonkey.review.datastructure.sort.ArithmeticUtils.isSmallerThan;
+
 public class CombSort<N extends Number> implements Sort<N> {
 
-    @Override
-    public N[] sort(N[] unordered) {
+    private static final double SHRINK = 1.3;
+    private final BubbleSort<N> bubbleSort = new BubbleSort<>();
 
-        return null;
+    @Override
+    public N[] sort(N[] array) {
+        int gap = array.length - 1;
+        do {
+            for (int i = 0; i + gap < array.length; i++) {
+                if (isSmallerThan(array[i + gap], array[i])) {
+                    N temp = array[i];
+                    array[i] = array[i + gap];
+                    array[i + gap] = temp;
+                }
+            }
+            gap /= SHRINK;
+        } while (gap > 1);
+        return bubbleSort.sort(array);
     }
 
 }
